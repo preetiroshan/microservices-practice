86865ab27adafa4a10f0f911dd5ce234d7b2bcb3
Preeti Roshan <preetiroshan14@gmail.com>
2023-09-16 01:28:01 +0530

    basic event flow setup

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,22 +10,35 @@ app.use(cors());
 
 const posts = {};
 app.get("/", (req, res) => {
-  res.send("Hello World! 2");
+  res.send("Hello World!");
 });
 
-app.listen(4000, () => {
-  console.log(`Example app listening on port 4000`);
-});
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const { title } = req.body;
   const id = randomBytes(4).toString("hex");
   posts[id] = {
     id,
     title,
   };
+  await axios.post("http://localhost:4004/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
   res.send(posts[id]);
+});
+
+app.post("/events", async (req, res) => {
+  console.log("Event Received in post service", req.body);
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log(`Example app listening on port 4000`);
 });
